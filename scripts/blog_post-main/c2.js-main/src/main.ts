2e506c1c9d6160dd23bddd8e131a8e164d1dc4cf
NICALOD3ON <129828101+NICALOD3ON@users.noreply.github.com>
2024-05-11 16:34:25 +1000

    undo

--- scripts/blog_post-main/c2.js-main/src/main.ts
+++
@@ -1,6 +0,0 @@
-export * from './math';
-export * from "./geometry";
-export * from './physics';
-export * from './evolution';
-export * from './audio';
-export * from './visual';
--- scripts/blog_post-main/posts/Homework_Week_5.md
+++
@@ -1,5 +0,0 @@
-# Homework Week 5
-### 2024-04-10
-#### The Phenomenology of Glitch Art
-
-#### Glitch Feminism: Glitch is a Skin
